fbdb9265bbdf215dae911275fc78f4b815296a6e
salrey <tekdehn@gmail.com>
2022-02-18 19:38:48 -0500

    App dependencies, backend setup & heroku deployment

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,8 +1,18 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO 
+    storeapp
+        (name, developer, image, price, description, category, age, featured) 
+VALUES
+    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', true),
+    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', true),
+    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', true),
+    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', true),
+    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', true);
+
+INSERT INTO 
+    reviews (storeapp_id, reviewer, title, content, rating)
+VALUES 
+    ('1', 'Charlie', 'Action-Packed!', 'This is probably the best game series ever created', 4),
+    ('3', 'Carly', 'Classic', 'I''m really happy that many of the classic levels were brought back', 3),
+    ('2', 'Colin', 'Biggest Fan', 'It truly feels like a remaster', 5),
+    ('5', 'Carlos', 'They''re Back and Better', 'This is a classic, pig-popping, bird-flinging fun!', 5),
+    ('4', 'Cindy', 'Timeless', 'Plays like the AB that we''ve loved for years', 4);
