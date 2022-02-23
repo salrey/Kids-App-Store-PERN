021017644a685fdfef0595496f45afc0f11a8a7e
salrey <tekdehn@gmail.com>
2022-02-23 12:31:40 -0500

    Update images in seed

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -2,11 +2,11 @@ INSERT INTO
     storeapp
         (name, developer, image, price, description, category, age, featured) 
 VALUES
-    ('Angry Birdz Reloaded', 'Rovio', 'https://placeit.net/b3bcfacf54fb32', 5, 'The world''s most famous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', false),
-    ('Zoombinis', 'Encore', 'https://placeit.net/cc91d903e4f66f', 10, 'Zoombinis is a re-creation of the classic, award-winning 1990''s puzzle game Logical Journey of the Zoombinis', 'Education', '9+', true),
-    ('Music World Karaoke', 'Foriero', 'https://placeit.net/1d2dbb6108c114', 4, 'Beautifully rendered the most popular children songs with lyrics text and jumping star', 'Music', '3+', true),
-    ('Typing Fingers LT', 'Foriero', 'https://placeit.net/85462ee7d1535f', 2, 'Typing Fingers uses a completely new approach to teach the efficient touch-typing (ten-fingers) system', 'Education', '3+', true),
-    ('Amazon Prime Video', 'AMZN Mobile', 'https://placeit.net/723023c545163a', 0, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Entertainment', '12+', false);
+    ('Angry Birdz Reloaded', 'Rovio', 'https://cdn.pixabay.com/photo/2020/05/16/00/25/wallpapers-5175518_1280.png', 5, 'The world''s most famous flock has returned in a new version of the mobile game that took the world by storm', 'Games', '3+', false),
+    ('Zoombinis', 'Encore', 'https://cdn.pixabay.com/photo/2012/04/11/17/31/vegetables-29063_1280.png', 10, 'Zoombinis is a re-creation of the classic, award-winning 1990''s puzzle game Logical Journey of the Zoombinis', 'Education', '9+', true),
+    ('Music World Karaoke', 'Foriero', 'https://cdn.pixabay.com/photo/2016/06/16/08/42/monster-1460885_1280.png', 4, 'Beautifully rendered the most popular children songs with lyrics text and jumping star', 'Music', '3+', true),
+    ('Typing Fingers LT', 'Foriero', 'https://cdn.pixabay.com/photo/2016/06/15/16/16/man-1459246_1280.png', 2, 'Typing Fingers uses a completely new approach to teach the efficient touch-typing (ten-fingers) system', 'Education', '3+', true),
+    ('Amazon Prime Video', 'AMZN Mobile', 'https://cdn.pixabay.com/photo/2015/12/03/01/27/play-1073616_1280.png', 0, 'The world''s most famoous flock has returned in a new version of the mobile game that took the world by storm', 'Entertainment', '12+', false);
 
 INSERT INTO 
     reviews (storeapp_id, reviewer, title, content, rating)
